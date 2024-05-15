1a5e424f4b63584781bf1463c889f0fadc6977e7
student <b203120708@gmail.com>
2024-05-15 16:11:09 +0800

    v0

---
+++ app/src/main/java/com/example/a0515/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.a0515
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.a0515.ui.theme._0515Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            _0515Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    //Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun PointerEvents() {
+    var msg by remember { mutableStateOf("") }
+    Column {
+        Text(msg)
+        Box(
+            Modifier
+                .fillMaxSize()
+                .background(Color.Yellow)
+                .pointerInput(Unit) {
+                    awaitPointerEventScope {
+                        while (true) {
+                            val event = awaitPointerEvent()
+                            msg = "${event.type}, ${event.changes.first().position}"
+                        }
+                    }
+                }
+        )
+    }
+}
